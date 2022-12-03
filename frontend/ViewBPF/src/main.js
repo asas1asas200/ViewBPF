56a478a7c5176353e820a023e768de07ae8a67f9
asas1asas200 <asas1asas200@gmail.com>
2022-12-03 16:55:15 +0800

    feat(vue): add vue router

--- frontend/ViewBPF/src/main.js
+++ frontend/ViewBPF/src/main.js
@@ -4,8 +4,10 @@ import "element-plus/dist/index.css";
 import App from "./App.vue";
 
 import "./assets/main.css";
+import router from "./router";
 
 const app = createApp(App);
 
+app.use(router);
 app.use(ElementPlus);
 app.mount("#app");

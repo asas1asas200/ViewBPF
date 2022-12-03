7d4aae20b90c9cf0e03e66d1d3985d235e5faed4
asas1asas200 <asas1asas200@gmail.com>
2022-12-04 07:44:48 +0800

    feat(frontend): add active link in sidebar

--- frontend/ViewBPF/src/components/Sidebar.vue
+++ frontend/ViewBPF/src/components/Sidebar.vue
@@ -1,39 +1,19 @@
 <template>
-  <el-menu style="height: 100vh;" active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo" default-active="2"
-    text-color="#fff" @open="handleOpen" @close="handleClose">
-    <el-sub-menu index="1">
-      <template #title>
-        <el-icon>
-          <location />
-        </el-icon>
-        <span>Navigator One</span>
-      </template>
-      <el-menu-item-group title="Group One">
-        <el-menu-item index="1-1">item one</el-menu-item>
-        <el-menu-item index="1-2">item two</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="Group Two">
-        <el-menu-item index="1-3">item three</el-menu-item>
-      </el-menu-item-group>
-      <el-sub-menu index="1-4">
-        <template #title>item four</template>
-        <el-menu-item index="1-4-1">item one</el-menu-item>
-      </el-sub-menu>
-    </el-sub-menu>
-    <el-menu-item index="2">
-      <el-icon><icon-menu /></el-icon>
-      <span>Navigator Two</span>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <el-icon>
-        <document />
-      </el-icon>
-      <span>Navigator Three</span>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <el-icon>
-        <setting />
-      </el-icon>
+  <el-menu style="height: 100vh;" active-text-color="#ffd04b" background-color="#545c64" class="el-menu-vertical-demo" text-color="#fff" :default-active="currentPath">
+    <router-link to="/">
+      <el-menu-item index="/">
+        <el-icon><icon-menu /></el-icon>
+        <span>Navigator Two</span>
+      </el-menu-item>
+    </router-link>
+    <router-link to="/about">
+      <el-menu-item index="/about">
+        <el-icon><document /></el-icon>
+        <span>Navigator Three</span>
+      </el-menu-item>
+    </router-link>
+    <el-menu-item index="3">
+      <el-icon><setting /></el-icon>
       <span>Navigator Four</span>
     </el-menu-item>
   </el-menu>
@@ -43,13 +23,20 @@
 import {
   Document,
   Menu as IconMenu,
-  Location,
   Setting,
 } from '@element-plus/icons-vue'
-const handleOpen = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
-const handleClose = (key: string, keyPath: string[]) => {
-  console.log(key, keyPath)
-}
+
+import { useRoute } from 'vue-router'
+import { watch, ref } from 'vue'
+
+const route = useRoute()
+const currentPath = ref(route.path)
+
+//FIXME: The path will change twice when the page is refreshed
+watch(
+  () => route.path,
+  path => {
+    currentPath.value = path
+  },
+)
 </script>
